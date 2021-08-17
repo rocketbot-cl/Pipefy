3056d68d685d1533e364114570dc3a3afd3caa6a
DaniloToroL <danilo.toro@usach.cl>
2021-08-17 14:46:07 -0400

    [new] add manual and readme

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Pipefy
+  
+Module to automate pipefy through its API  
+
+## Overview
+
+
+1. #### Connect to Pipefy  
+With this command the authentication against Pipefy is established
+
+2. #### Create Card  
+With this command can create a card
+
+3. #### Get Pipe by ID  
+With this command you can obtain information about the fields of a pipe from its id
+
+----
+### OS
+
+- windows
+- mac
+- linux
+- docker
+
+### Dependencies
+- [**requests**](https://pypi.org/project/requests/)
+### License
+  
+![MIT](https://camo.githubusercontent.com/107590fac8cbd65071396bb4d04040f76cde5bde/687474703a2f2f696d672e736869656c64732e696f2f3a6c6963656e73652d6d69742d626c75652e7376673f7374796c653d666c61742d737175617265)  
+[MIT](http://opensource.org/licenses/mit-license.ph)
--- libs/pipefy.py
+++ libs/pipefy.py
@@ -40,8 +40,8 @@ class Pipefy:
         input_args = {"pipe_id": pipe_id,
                       "fields_attributes": fields_without_quote}
         if upload_attachments:
-            filename = self.upload_file(upload_attachments)
-            input_args["attachments"] =  filename
+            filenames = self.upload_file(upload_attachments)
+            input_args["attachments"] =  filenames
         input_args = removeQuotes(input_args)
         query = "mutation{createCard(input:input_args) options}".replace(
             "input_args", input_args).replace("options", options)
@@ -49,9 +49,10 @@ class Pipefy:
         return self.__requests("post", {"query": query})
 
     def upload_files(self, attachments: list):
+        filenames = []
         for attachment in attachments:
             if os.path.isfile(attachment):
-                self.upload_files(attachment)
+                filenames.append(self.upload_files(attachment))
 
     def upload_file(self, file: str) -> str:
         filename = os.path.basename(file)
@@ -59,7 +60,6 @@ class Pipefy:
         request = requests.request("get", url)
         response = request.json()
         res = requests.request("put", response["signedUrl"], data=open(file, 'rb'))
-        
         return response["filename"]
 
     def __requests(self, method: str, query: dict = {}) -> dict:
@@ -70,11 +70,3 @@ class Pipefy:
         response = requests.request(
             method, self.api_url, json=query, headers=headers)
         return response.json()
-
-
-pipefy = Pipefy("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDEzMDY4NzYsImVtYWlsIjoiZGFuaWxvLnRvcm9Acm9ja2V0Ym90LmNvbSIsImFwcGxpY2F0aW9uIjozMDAxMDcxODF9fQ.waS9k2ekcQq5xkQ5JU63vfNiv2VXTyHmSmsPX4ulQ1WnVwRz7Dka_kYAjHu7vN8k4_KKRcM7hVivhR1sr7RnCA")
-
-# print(pipefy.get_all_cards(301741450,"{edges {node {id,assignees {id}}}}"))
-# print(pipefy.get_pipe_by_id(301741450,"{start_form_fields {id, label}}"))
-print(pipefy.create_card(301741450, [{"field_id": "nome_do_solicitante", "field_value": "Danilo2"}, {"field_id": "e_mail_do_solicitante", "field_value": "Danilo@rocketbot.com"}, {
-      "field_id": "tipo_de_contrato", "field_value": "Indeterminado"}, {"field_id": "contrato_j_criado", "field_value": "Sim"}], upload_attachments="C:/Users/danil/Downloads/RPA_Integration.pdf"))
